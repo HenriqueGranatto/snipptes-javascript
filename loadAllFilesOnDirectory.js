73a123df50617151758d95112d351bf131d8efa1
Henrique Granatto <henrique.ramires.granatto@gmail.com>
2019-11-24 10:49:20 -0300

    Add files via upload

---
+++ loadAllFilesOnDirectory.js
@@ -0,0 +1,37 @@
+/** Carrega todos os arquivos de todos os diretórios a partir do atual (ondeo método foi chamado) */
+loadAllFilesOnDirectory()
+{
+    try
+    {
+        fs.readdir(`${__dirname}`, function (err, directories) 
+        {
+            if (err) { return false } 
+
+            directories.push(__dirname)
+
+            directories.forEach(function(directory){
+                if(directory.split(".").length === 1)
+                {
+                    directory = (__dirname !== directory) ? `${__dirname}\\${directory}` : __dirname
+
+                    fs.readdir(`${directory}`, function (err, files) 
+                    {
+                        if (err) { return false } 
+            
+                        files.forEach(function (file) 
+                        {
+                            if(file.split(".").length ===2)
+                            {
+                                require(`${directory}\\${file}`)
+                            }
+                        });
+                    });       
+                }
+            })
+        })   
+    }
+    catch(e)
+    {   
+        throw e
+    }
+}
